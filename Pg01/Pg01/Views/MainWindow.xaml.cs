be4026d0494fd1bc383974673e971074d73bb78c
ycums <boobootmp00@gmail.com>
2016-11-05 21:43:28 +0900

    closes #9  タスクバーアイコンから終了できるようにする closes #8  タスクバーに常駐する機能を追加 closes #7  クリックしてもフォーカスされないウィンドウにする

--- Pg01/Pg01/Views/MainWindow.xaml.cs
+++ Pg01/Pg01/Views/MainWindow.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
+using Application = System.Windows.Application;
 
 namespace Pg01.Views
 {
@@ -23,13 +14,108 @@ namespace Pg01.Views
      */
 
     /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
+    ///     MainWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private NotifyIcon _notifyIcon;
+
         public MainWindow()
         {
+            Application_Startup();
+
             InitializeComponent();
+
+            ShowInTaskbar = false;
+            WindowStyle = WindowStyle.None;
+            AllowsTransparency = true;
+            Topmost = true;
+            InitNotifyIcon();
+            CaptureMouse();
         }
+
+        private void Application_Startup()
+        {
+            var ipc = new IpcMessageSender<string>("RadGest", "RadGest01Startup");
+
+            if (ipc.IsCadet)
+            {
+                Console.WriteLine(Properties.Resources.MainWindow_Application_Startup_);
+                ipc.SendMessage($"{DateTime.Now:HH時mm分ss秒}です。後続プロセスが起動しました。");
+                Environment.Exit(1);
+            }
+            else
+            {
+                // IPCでオブジェクトを受信したとき発生します。
+                Console.WriteLine(Properties.Resources.MainWindow_Application_Startup_先行プロセス);
+                ipc.MessageReceived += ipc_MessageReceived;
+            }
+            Console.ReadLine();
+        }
+
+
+        private void InitNotifyIcon()
+        {
+            _notifyIcon = new NotifyIcon
+            {
+                Text = Properties.Resources.NotifyIconText,
+                Icon = Properties.Resources.PerfCenterCpl,
+                Visible = true
+            };
+
+            var menuStrip = new ContextMenuStrip();
+
+            var configItem = new ToolStripMenuItem {Text = Properties.Resources.NotifyIconconfigItemText};
+            menuStrip.Items.Add(configItem);
+            configItem.Click += configItem_Click;
+
+            var exitItem = new ToolStripMenuItem {Text = Properties.Resources.NotifyIconExitItemText};
+            menuStrip.Items.Add(exitItem);
+            exitItem.Click += exitItem_Click;
+
+            _notifyIcon.ContextMenuStrip = menuStrip;
+            _notifyIcon.MouseClick += _notifyIcon_MouseClick;
+        }
+
+        #region "IPC Events"
+
+        private void ipc_MessageReceived(object sender, MessageEventArgs<string> e)
+        {
+            Dispatcher.Invoke(Show);
+        }
+
+        #endregion
+
+        #region "NotifiIcon Events"
+
+        private void _notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        private void exitItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _notifyIcon.Dispose();
+                Application.Current.Shutdown();
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        private void configItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
     }
 }
